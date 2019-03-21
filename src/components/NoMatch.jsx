bb99f0d872e31b29ef2d416fb78ae01a1e68c247
mikeyuchima <tomichaels@gmail.com>
2019-03-21 14:10:53 -0400

    edited css

--- src/components/NoMatch.jsx
+++ src/components/NoMatch.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 const NoMatch = () => (
-  <div class="main">
-    <div class="error-page">
-      <h1>404</h1>
-      <h1>ERROR</h1>
-      <p>PAGE NOT FOUND</p>
+  <div className="background">
+    <div class="main">
+      <div class="error-page">
+        <h1>404</h1>
+        <h1>ERROR</h1>
+        <p>PAGE NOT FOUND</p>
+      </div>
     </div>
   </div>
 );
--- src/components/Strip.jsx
+++ src/components/Strip.jsx
@@ -4,8 +4,8 @@ const Strip = props => (
   <div className="Strip">
     <h1> {props.title} </h1>
     <img src={props.img} />
-    <span>
-      {props.date} #{props.issue}
+    <span className="Description">
+      Date Released: {props.date} Issue# {props.issue}
     </span>
     <p id="transcript">{props.transcript}</p>
   </div>
